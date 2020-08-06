c43ae67fa1c67b27db840cc6e10fc9799e9f4f1e
Sebastian Higuita <sebastianhiguita@Sebastians-MacBook-Air.local>
2020-08-05 20:16:44 -0500

    nodejs-jwt-refresh-authentication commit

---
+++ Routes/Auth.route.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const router = Router();
+const AuthController = require("../Controllers/Auth.Controller");
+
+router.post("/register", AuthController.register);
+
+router.post("/login", AuthController.login);
+
+router.post("/refresh-token", AuthController.refreshToken);
+
+router.delete("/logout", AuthController.logout);
+
+module.exports = router;
